113ee19866b95c3fa6edf08ccf038573b1896507
Eric Liu <eliu1@andrew.cmu.edu>
2021-02-05 22:48:47 -0500

    points_earned method and 100% test coverage

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -11,10 +11,14 @@ class ChildTest < ActiveSupport::TestCase
   context "Creating a child context" do
     setup do
       create_children
+      create_tasks
+      create_chores
     end
  
     teardown do
       destroy_children
+      destroy_tasks
+      destroy_chores
     end
  
     should "have name methods that list first_ and last_names combined" do
@@ -29,6 +33,12 @@ class ChildTest < ActiveSupport::TestCase
  
     should "have a scope to select only active children" do
       assert_equal ["Alex", "Mark"], Child.active.alphabetical.map{|c| c.first_name}
+    end
+
+    should "have a points_earned method" do
+      assert_equal 4, @alex.points_earned
+      assert_equal 1, @mark.points_earned
+      assert_equal 0, @rachel.points_earned
     end
   end
 end
