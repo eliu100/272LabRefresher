3fb0d532ca7d6a40e479ff2822fc7a3c11e0c92f
Eric Liu <eliu1@andrew.cmu.edu>
2021-02-05 22:18:50 -0500

    tasks test all pass

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,8 +2,8 @@ class Child < ApplicationRecord
     has_many :chores
     has_many :tasks, through: :chores
 
-    validates_presence_of(:first_name)
-    validates_presence_of(:last_name)
+    validates_presence_of :first_name
+    validates_presence_of :last_name
 
     def name
         "#{first_name} #{last_name}"
